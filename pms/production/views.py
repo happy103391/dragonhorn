d6ba9c8dd226807572c4676320e7c2776214dd0a
Peggy1206 <44251722+Peggy1206@users.noreply.github.com>
2020-01-16 01:16:25 +0800

    刪doc

--- pms/production/views.py
+++ pms/production/views.py
@@ -6,7 +6,7 @@ from django.views.generic.list import ListView
 from django import forms
 from django.http import HttpResponseRedirect
 from django.db.models import F
-from docutils.parsers import null
+# from docutils.parsers import null
 from django.db.models import Sum
 import datetime
 import math
@@ -25,10 +25,10 @@ import sqlite3
 #     return render(request,'rfm.html')
 # def data(request):
 #     return render(request,'data.html')
-def historyrecord(request):
-    return render(request,'historyrecord.html')
-def inventory(request):
-    return render(request,'inventory.html')
+# def historyrecord(request):
+#     return render(request,'historyrecord.html')
+# def inventory(request):
+#     return render(request,'inventory.html')
 # def create_inventory(request):
 #     return render(request,'create_inventory.html')
 
@@ -43,7 +43,7 @@ def inventory(request):
         sell_pd3 = request.POST['sell_pd3']
         sell_pd4 = request.POST['sell_pd4']
         sell_pd5 = request.POST['sell_pd5']
-        unit = DailyRecord.objects.create(sell_date=sell_date, sell_pd1=sell_pd1, sell_pd2=sell_pd2, sell_pd3=sell_pd3, sell_pd4=sell_pd4, sell_pd5=sell_pd5) 
+        unit = DailyRecord.objects.create(sell_date=sell_date, sell_pd1=sell_pd1, sell_pd2=sell_pd2, sell_pd3=sell_pd3, sell_pd4=sell_pd4, sell_pd5=sell_pd5)
         unit.save()                      #寫入資料庫
         return redirect('home/inventory/')       #重導到<index.html>網頁
     else:
@@ -53,10 +53,10 @@ def inventory(request):
 
 
 def inventory(request):
-    
-    
+
+
     use_list = Matirial.objects.all().order_by('m_number')
-    
+
     class Meta:
         model = Matirial
         fields = ['m_number', 'm_name','m_left_amount','m_safe_inventory',]
@@ -69,43 +69,43 @@ class CurrentUseForm(HttpResponseRedirect):
         latest_record = DailyRecord.objects.latest('id')
         used_blacktea= (latest_record.sell_pd1 + latest_record.sell_pd2 + latest_record.sell_pd3 )*300
         Matirial.objects.all().filter(m_name='紅茶（ml）').update(m_left_amount=F('m_left_amount') - used_blacktea)
-        
-        
+
+
         used_milk =(latest_record.sell_pd1 + latest_record.sell_pd2 + latest_record.sell_pd3 + latest_record.sell_pd4 + latest_record.sell_pd5)*200
         Matirial.objects.all().filter(m_name='牛奶（ml）').update(m_left_amount=F('m_left_amount') - used_milk)
-        
+
         used_pearl =(latest_record.sell_pd3 + latest_record.sell_pd5)*20
         Matirial.objects.all().filter(m_name='珍珠（g）').update(m_left_amount=F('m_left_amount') - used_pearl)
-        
+
         used_taro =(latest_record.sell_pd2)*20
         Matirial.objects.all().filter(m_name='芋圓（g）').update(m_left_amount=F('m_left_amount') - used_taro)
-        
+
         used_honey =(latest_record.sell_pd4 + latest_record.sell_pd5)*100
         Matirial.objects.all().filter(m_name='蜂蜜（g）').update(m_left_amount=F('m_left_amount') - used_honey)
-        
-    
+
+
 class AddUseForm(request):
     class Meta:
         model = Matirial
         fields = ['m_number', 'm_name','m_left_amount','m_safe_inventory',]
         latest_add_record = addMaterial.objects.latest('add_date')
-        new_blacktea= latest_add_record.add_blacktea*30000 
+        new_blacktea= latest_add_record.add_blacktea*30000
         Matirial.objects.all().filter(m_name='紅茶（ml）').update(m_left_amount=F('m_left_amount') + new_blacktea)
         new_milk = latest_add_record.add_milk*5000
         Matirial.objects.all().filter(m_name='牛奶（ml）').update(m_left_amount=F('m_left_amount') + new_milk)
-        
+
         new_pearl = latest_add_record.add_pearl*3000
         Matirial.objects.all().filter(m_name='珍珠（g）').update(m_left_amount=F('m_left_amount') + new_pearl)
 
         new_taro = latest_add_record.add_taro*3000
         Matirial.objects.all().filter(m_name='芋圓（g）').update(m_left_amount=F('m_left_amount') + new_taro)
-        
+
         new_honey =latest_add_record.add_honey*3000
         Matirial.objects.all().filter(m_name='蜂蜜（g）').update(m_left_amount=F('m_left_amount') + new_honey)
-        
-        
 
-    
+
+
+
 
 def historyrecord(request):           #歷史紀錄，按照日期排序
     # drecord_list = DailyRecord.objects.all().order_by('-sell_date')
@@ -120,7 +120,7 @@ def historyrecord(request):           #歷史紀錄，按照日期排序
     if query and request.method == 'GET':
         results = DailyRecord.objects.filter(sell_date=query)
         context.update({'results': results})
-    
+
     return render(request,'historyrecord.html',context)
 
 #def CalculateTodayConsume(request):
@@ -132,10 +132,10 @@ def historyrecord(request):           #歷史紀錄，按照日期排序
 class TodaySellForm(forms.ModelForm):
     class Meta:
         model = DailyRecord
-        fields = ['sell_date', 'sell_pd1','sell_pd2','sell_pd3','sell_pd4','sell_pd5', ]               
+        fields = ['sell_date', 'sell_pd1','sell_pd2','sell_pd3','sell_pd4','sell_pd5', ]
 
 
-    
+
 
 # def material(request):
 #     drecord_list = DailyRecord.objects.all().order_by('-sell_date')
@@ -145,7 +145,7 @@ class TodaySellForm(forms.ModelForm):
 #     return render(request,'material.html',locals())
 
 def material(request):                             #讓使用者查詢，並列出預測的量
-     
+
     query = request.GET.get('search_res', None)
     context = {}
     class Meta:
@@ -158,16 +158,16 @@ def material(request):                             #讓使用者查詢，並列�
         use_list = Matirial.objects.all().order_by('m_number')
 
         # spring_start_date = datetime.date(2019, 1, 1)                  #春天的銷量
-        # spring_end_date =  datetime.date(2019, 3, 31) 
+        # spring_end_date =  datetime.date(2019, 3, 31)
         # DailyRecord.sell_date.strftime('%Y-%m-%d')
         # spring = DailyRecord.objects.filter(sell_date=[spring_start_date, spring_end_date])
-        for r in results:  
+        for r in results:
                 r.predicted_sell＿pd1 =math.ceil((r.sell_pd1)* 0.9)     #預測的銷售量
                 r.predicted_sell＿pd2 =math.ceil((r.sell_pd2)* 1.1)
                 r.predicted_sell＿pd3 =math.ceil((r.sell_pd3)* 1.2)
                 r.predicted_sell＿pd4 =math.ceil((r.sell_pd4)* 0.9)
                 r.predicted_sell＿pd5 =math.ceil((r.sell_pd5)* 0.8)
-                
+
                 r.predicted_blacktea_usage = math.ceil(r.sell_pd1 + r.sell_pd2 + r.sell_pd3 )*300     #預測的原料用量
                 r.predicted_milk_usage = math.ceil(r.sell_pd1 + r.sell_pd2 + r.sell_pd3 + r.sell_pd4 + r.sell_pd5 )*200
                 r.predicted_pearl_usage = math.ceil(r.sell_pd3 + r.sell_pd5 )*20
@@ -180,21 +180,21 @@ def material(request):                             #讓使用者查詢，並列�
                 r.predicted_taro_unit = round(((r.sell_pd2)*20/3000),2)
                 r.predicted_honey_unit = round(((r.sell_pd4 + r.sell_pd5 )*100/3000),2)
 
-        
-        
+
+
                 #r.need_to_buy_milk = math.ceil((use_list.m_safe_inventory - use_list.m_left_amount + r.predicted_milk_usage)/30000)
 
         model = Matirial
         fields = ['m_number', 'm_name','m_left_amount','m_safe_inventory',]
-        for u in use_list: 
+        for u in use_list:
             u.need_to_buy_blacktea = math.ceil((Matirial.objects.get(m_number ='1').m_safe_inventory - Matirial.objects.get(m_number ='1').m_left_amount + r.predicted_blacktea_usage)/30000)
             u.need_to_buy_milk = math.ceil((Matirial.objects.get(m_number ='2').m_safe_inventory - Matirial.objects.get(m_number ='2').m_left_amount + r.predicted_milk_usage)/5000)
-                
+
             u.need_to_buy_pearl = math.ceil((Matirial.objects.get(m_number ='3').m_safe_inventory - Matirial.objects.get(m_number ='3').m_left_amount + r.predicted_pearl_usage)/3000)
             u.need_to_buy_taro = math.ceil((Matirial.objects.get(m_number ='4').m_safe_inventory - Matirial.objects.get(m_number ='4').m_left_amount + r.predicted_taro_usage)/3000)
             u.need_to_buy_honey = math.ceil((Matirial.objects.get(m_number ='5').m_safe_inventory - Matirial.objects.get(m_number ='5').m_left_amount + r.predicted_honey_usage)/3000)
     return render(request,'material.html', locals())
-                
+
 
         # r.need_to_buy_blacktea = math.ceil((Matirial.objects.get(m_number ='1').m_safe_inventory - Matirial.objects.get(m_number ='1').m_left_amount + r.predicted_blacktea_usage)/30000)
         # r.need_to_buy_milk = math.ceil((Matirial.objects.get(m_number ='2').m_safe_inventory - Matirial.objects.get(m_number ='2').m_left_amount + r.predicted_milk_usage)/5000)
@@ -202,11 +202,11 @@ def material(request):                             #讓使用者查詢，並列�
         # r.need_to_buy_taro = math.ceil((Matirial.objects.get(m_number ='4').m_safe_inventory - Matirial.objects.get(m_number ='4').m_left_amount + r.predicted_taro_usage)/3000)
         # r.need_to_buy_honey = math.ceil((Matirial.objects.get(m_number ='5').m_safe_inventory - Matirial.objects.get(m_number ='5').m_left_amount + r.predicted_honey_usage)/3000)
 
-        
+
         # Matirial.objects.all().filter(m_name='紅茶（ml）').update(m_left_amount=F('m_left_amount')/30000)
         # for a in use_list:
         #     a.predictday_blacktea = math.ceil((a.m_left_amount - a.m_safe_inventory)/r.predicted_blacktea_usage)
-    
+
 
     # class Meta:                 #沒用的東西
     #     model = Matirial
@@ -217,16 +217,16 @@ def material(request):                             #讓使用者查詢，並列�
     # return render(request,'material.html', locals())
 
 
- 
+
 # month_list = DailyRecord.objects.dates('sell_date', 'month')
         # for results.sell_date in month_list:
         #     print(results)
-        
+
         # spring = DailyRecord.objects.filter(sell_date__range=(spring_start_date, spring_end_date))
         #  for s in spring :
         #      spring_result_pd1 = s.sell_pd1*0.9
-        #      print(spring_result_pd1)       
-    
+        #      print(spring_result_pd1)
+
 
 
 
@@ -250,7 +250,7 @@ def material(request):                             #讓使用者查詢，並列�
     #      sell_pd3 = request.POST['濃厚珍珠奶茶 銷量：']
     #      sell_pd4 = request.POST['特濃蜂蜜牛奶 銷量：']
     #      sell_pd5 = request.POST['蜂蜜珍珠鮮奶 銷量：']
-    #      sellrecord = DailyRecord.objects.create(sell_date=sell_date, sell_pd1=sell_pd1, sell_pd2=sell_pd2, sell_pd3=sell_pd3, sell_pd4=sell_pd4, sell_pd5=sell_pd5) 
+    #      sellrecord = DailyRecord.objects.create(sell_date=sell_date, sell_pd1=sell_pd1, sell_pd2=sell_pd2, sell_pd3=sell_pd3, sell_pd4=sell_pd4, sell_pd5=sell_pd5)
     #      sellrecord.save()                      #寫入資料庫
     #      return redirect('/inventory/')       #重導到<index.html>網頁
     #  else:
